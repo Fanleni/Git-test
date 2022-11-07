21c6b92c6e7e9e75f40400fab2b2296de0533b8a
nini <1332446345@qq.cim>
2022-11-07 15:59:16 +0800

    four commit0.0

--- src/main/java/com/nini/GitTest.java
+++ src/main/java/com/nini/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("我真的很桑心,别桑心了嘛,ok?~。。。!");
-        System.out.println("啦啦啦，第三次提交了,失败了，再来一次");
+        System.out.println("啦啦啦，第三次提交了,失败了，再来一次..哦哦");
     }
 }
