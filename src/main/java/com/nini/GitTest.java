378d6aa109683723cfb7257fec68c0649aff660d
nini <1332446345@qq.cim>
2022-11-07 15:36:05 +0800

    hot-fix commit0.0

--- src/main/java/com/nini/GitTest.java
+++ src/main/java/com/nini/GitTest.java
@@ -7,6 +7,6 @@ package com.nini;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("我真的很桑心,别桑心了嘛~。。。!");
+        System.out.println("我真的很桑心,别桑心了嘛,ok?~。。。!");
     }
 }
