f5e70c5690988268d4c953767e2f4d6b156cb367
nini <1332446345@qq.cim>
2022-11-07 15:23:17 +0800

    first commit0.0

---
+++ src/main/java/com/nini/GitTest.java
@@ -0,0 +1,11 @@
+package com.nini;
+
+/**
+ * @author nini
+ * @create 2022--11--07--15:17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
